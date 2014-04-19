6468e69a9a9f916e2c79229f8be29bc84d133bfb
Kenny Bastani <kb@socialmoon.com>
2014-04-18 23:28:25 -0700

    Ported the .NET scheduler to Node.js
    Created an import scheduler in Node.js based on the previous .NET C#
    version.

--- scheduler/app.js
+++ scheduler/app.js
@@ -4,9 +4,12 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var importer = require('./import/importer');
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var url = require("url");
+var _ = require('underscore');
+var meetup = require('meetup-api')('4b1b7a4c4027718192d1e73723135b');
 
 var app = express();
 
@@ -63,15 +66,256 @@ var server = app.listen(3001, function() {
 module.exports = app;
 
 
-//
+function writeResponse (res, response, start) {
+  res.header('Duration-ms', new Date() - start);
+  if (response.neo4j) {
+    res.header('Neo4j', JSON.stringify(response.neo4j));
+  }
+  res.send(JSON.stringify(response.results));
+}
+
+function parseUrl(req, key) {
+  return url.parse(req.url,true).query[key];
+}
+
+function parseBool (req, key) {
+  return 'true' == url.parse(req.url,true).query[key];
+}
+
 //0 5 0 * * * 
+//0 0-59 * * * *
 
 var CronJob = require('cron').CronJob;
-var job = new CronJob('0 0-59 * * * *', function(){
-    console.log('You will see this message every second');
+var job = new CronJob('0 5 0 * * *', function(){
+
+    var options = {
+      neo4j: "neo4j"
+    };
+
+    getPollingCities().forEach(function(city) {
+
+      meetup.getGroups({'topic' : 'NoSQL', 'country' : city.country, 'city' : city.city, 'state' : city.state, 'page' : '100'}, function(err,groups) {
+        console.log(city);
+        groups.results.forEach(function(group) {
+
+          // Get parameters from Meetup API
+          var params = getGroupImportParameters(group);
+          
+          // Format parameters for Cypher REST API
+          params = { 'csvLine' : params };
+
+          // Import group stats for day
+          importer.importGroupStats(params, options, function (err, response) {
+            console.log(response.results[0].name);
+          });
+
+        });
+        
+      });
+
+    });
+    
   }, function () {
-    // This function is executed when the job stops
+    
   },
   true /* Start the job right now */,
   "America/Los_Angeles" /* Time zone of this job. */
 );
+
+function getPollingCities()
+{
+  var cities = [
+    {
+        city: "New York",
+        state: "NY",
+        country: "US"
+    },
+    {
+        city: "Boston",
+        state: "MA",
+        country: "US"
+    },
+    {
+        city: "Atlanta",
+        state: "GA",
+        country: "US"
+    },
+    {
+        city: "Seattle",
+        state: "WA",
+        country: "US"
+    },
+    {
+        city: "Los Angeles",
+        state: "CA",
+        country: "US"
+    },
+    {
+        city: "San Francisco",
+        state: "CA",
+        country: "US"
+    },
+    {
+        city: "Chicago",
+        state: "IL",
+        country: "US"
+    },
+    {
+        city: "Austin",
+        state: "TX",
+        country: "US"
+    },
+    {
+        city: "Dallas",
+        state: "TX",
+        country: "US"
+    },
+    {
+        city: "Denver",
+        state: "CO",
+        country: "US"
+    },
+    {
+        city: "London",
+        state: "",
+        country: "GB"
+    },
+    {
+        city: "Paris",
+        state: "",
+        country: "FR"
+    },
+    {
+        city: "München",
+        state: "",
+        country: "DE"
+    },
+    {
+        city: "Berlin",
+        state: "",
+        country: "DE"
+    },
+    {
+        city: "Copenhagen",
+        state: "",
+        country: "DK"
+    },
+    {
+        city: "Stockholm",
+        state: "",
+        country: "SE"
+    },
+    {
+        city: "Malmö",
+        state: "",
+        country: "SE"
+    },
+    {
+        city: "Brussels",
+        state: "",
+        country: "BE"
+    },
+    {
+        city: "Frankfurt",
+        state: "",
+        country: "DE"
+    },
+    {
+        city: "Hamburg",
+        state: "",
+        country: "DE"
+    },
+    {
+        city: "Oslo",
+        state: "",
+        country: "NO"
+    }
+  ];
+
+  return cities;
+}
+
+function getGroupImportParameters(group)
+{
+
+  var createdDate = getDateTimeFromUnix(group.created);
+  var today = new Date();
+  var yesterday = new Date();
+  yesterday.setDate(yesterday.getDate() - 1);
+
+  var csvLine = {
+      group_name: group.name,
+      group_creation_date: group.created,
+      group_creation_date_year: createdDate.year,
+      group_creation_date_month: createdDate.month,
+      group_creation_date_day: createdDate.day,
+      group_location: group.city,
+      group_country: group.country,
+      group_state: group.state,
+      group_tag: group.topics.map(function(d) { return d.name; }),
+      last_month: yesterday.getMonth() + 1,
+      last_day: yesterday.getDate(),
+      last_year: yesterday.getFullYear(),
+      group_stats: group.members,
+      month: today.getMonth() + 1,
+      day: today.getDate(),
+      year: today.getFullYear(),
+      day_timestamp: getTicks(today),
+      day_of_week: today.getDay() + 1,
+      week_of_year: getWeekOfYear(today)
+  };
+
+  console.log(csvLine);
+
+  return csvLine;
+}
+
+function getTicks(dateTime)
+{
+
+  var ticks = ((dateTime.getTime() * 10000) + 621355968000000000);
+
+  return ticks;
+}
+
+function getWeekOfYear(d) {
+  // Attribution: https://gist.github.com/dblock/1081513
+
+  // Create a copy of this date object
+  var target = new Date(d.valueOf());
+  // ISO week date weeks start on monday
+  // so correct the day number
+  var dayNr = (d.getDay() + 6) % 7;
+   
+  // Set the target to the thursday of this week so the
+  // target date is in the right year
+  target.setDate(target.getDate() - dayNr + 3);
+   
+  // ISO 8601 states that week 1 is the week
+  // with january 4th in it
+  var jan4 = new Date(target.getFullYear(), 0, 4);
+   
+  // Number of days between target date and january 4th
+  var dayDiff = (target - jan4) / 86400000;
+   
+  // Calculate week number: Week 1 (january 4th) plus the
+  // number of weeks between target date and january 4th
+  var weekNr = 1 + Math.ceil(dayDiff / 7);
+   
+  return weekNr;
+}
+
+function getDateTimeFromUnix(unixTime)
+{
+  // create a new javascript Date object based on the timestamp
+  // multiplied by 1000 so that the argument is in milliseconds, not seconds
+  var date = new Date(unixTime);
+  // hours part from the timestamp
+  var day = date.getDate();
+  // minutes part from the timestamp
+  var month = date.getMonth() + 1;
+  // seconds part from the timestamp
+  var year = date.getFullYear();
+
+  return { day: day, month: month, year: year };
+}
---
+++ scheduler/import/importer.js
@@ -0,0 +1,76 @@
+/**
+ *  Meetup group statistics importer
+ *  these are mostly written in a functional style
+ */
+
+
+var _ = require('underscore');
+var uuid = require('hat'); // generates uuids
+var Cypher = require('../neo4j/cypher');
+var async = require('async');
+var randomName = require('random-name');
+
+/**
+ *  Result Functions
+ *  to be combined with queries using _.partial()
+ */
+
+var _resultsImportGroupStats = function (results, callback) {
+  var stats = _.map(results, function (result) {
+    var thisStats = {};
+    thisStats.name = result.name;
+    return thisStats;
+  });
+
+  callback(null, stats);
+};
+
+
+/**
+ *  Query Functions
+ *  to be combined with result functions using _.partial()
+ */
+
+var _importGroupStats = function (params, options, callback) {
+
+  var query = [
+      'MERGE (group:Group { name: { csvLine }.group_name })',
+      'ON CREATE SET group.created = toInt({ csvLine }.group_creation_date)',
+      'ON CREATE SET group.year = toInt({ csvLine }.group_creation_date_year)',
+      'ON CREATE SET group.month = toInt({ csvLine }.group_creation_date_month)',
+      'ON CREATE SET group.day = toInt({ csvLine }.group_creation_date_day)',
+      'MERGE (location:Location { city: { csvLine }.group_location, country: { csvLine }.group_country, state: { csvLine }.group_state })',
+      'FOREACH (name in { csvLine }.group_tag |',
+      '      MERGE (tag:Tag { tag: name }))',
+      'MERGE (stats:Stats { group_name: { csvLine }.group_name, month: toInt({ csvLine }.last_month), day: toInt({ csvLine }.last_day), year: toInt({ csvLine }.last_year) })',
+      'ON CREATE SET stats.count = toInt({ csvLine }.group_stats)',
+      'MERGE (day:Day { month: toInt({ csvLine }.month), day: toInt({ csvLine }.day), year: toInt({ csvLine }.year) })',
+      'ON CREATE SET day.timestamp = toInt({ csvLine }.day_timestamp)',
+      'ON CREATE SET day.dayofweek = toInt({ csvLine }.day_of_week)',
+      'MERGE (week:Week { year: toInt({ csvLine }.year), week: toInt({ csvLine }.week_of_year) })',
+      'MERGE (week)-[:HAS_DAY]->(day)',
+      'MERGE (month:Month { year: toInt({ csvLine }.year), month: toInt({ csvLine }.month) })',
+      'MERGE (month)-[:HAS_DAY]->(day)',
+      'MERGE (year:Year { year: toInt({ csvLine }.year) })',
+      'MERGE (year)-[:HAS_MONTH]->(month)',
+      'MERGE (lastDay:Day { month: toInt({ csvLine }.last_month), day: toInt({ csvLine }.last_day), year: toInt({ csvLine }.last_year) })',
+      'MERGE (group)-[:LOCATED_IN]->(location)',
+      'MERGE (group)-[:HAS_MEMBERS]->(stats)',
+      'MERGE (stats)-[:ON_DAY]->(day)',
+      'MERGE (lastDay)-[:NEXT]->(day)',
+      'RETURN group.name as name'].join('\n');
+
+  callback(null, query, params);
+};
+
+/**
+ *  Result Function Wrappers
+ *  a wrapper function that combines both the result functions with query functions
+ */
+
+var importGroupStats = Cypher(_importGroupStats, _resultsImportGroupStats);
+
+// export exposed functions
+module.exports = {
+  importGroupStats: importGroupStats
+};
