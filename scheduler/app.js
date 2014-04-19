0aff8f53d970d77f45627e3a0999aa91867af42a
Kenny Bastani <kb@socialmoon.com>
2014-04-19 02:02:56 -0700

    Optimized scheduler
    Removed unused dependencies, modified procfile for heroku deploy.

--- scheduler/app.js
+++ scheduler/app.js
@@ -1,69 +1,20 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 var importer = require('./import/importer');
-var routes = require('./routes/index');
-var users = require('./routes/users');
 var url = require("url");
 var _ = require('underscore');
 var meetup = require('meetup-api')('4b1b7a4c4027718192d1e73723135b');
-
+var express = require('express');
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(favicon());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-/// catch 404 and forwarding to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
+var env = process.env.NODE_ENV || 'development';
+if ('development' == env) {
+    app.use('/dist/assets', express.static(__dirname + '/dist/assets'));
+    app.use(express.static(__dirname + '/dist'));
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+var port = process.env.PORT || 3001;
+app.listen(port, function () {
+    console.log("Listening on " + port);
 });
-
-var server = app.listen(3001, function () {
-    console.log('Listening on port %d', server.address().port);
-});
-
-module.exports = app;
 
 
 function writeResponse(res, response, start) {
@@ -87,11 +38,8 @@ function parseBool(req, key) {
 
 var CronJob = require('cron').CronJob;
 var job = new CronJob('0 5 0 * * *', function () {
-
-
-        var cities = getPollingCities();
-        iteratorCityCallback(0, cities.length, cities);
-
+      var cities = getPollingCities();
+      iteratorCityCallback(0, cities.length, cities);
     }, function () {
 
     },
